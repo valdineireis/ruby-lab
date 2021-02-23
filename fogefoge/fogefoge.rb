77e558d1b4cf71f24b0cc504f4001a6fff60578b
Valdinei Reis <valdineimtz@gmail.com>
2021-02-22 21:50:56 -0300

    Refatoração, extraindo responsabilidades

--- fogefoge/fogefoge.rb
+++ fogefoge/fogefoge.rb
@@ -1,4 +1,5 @@
 require_relative 'ui'
+require_relative 'heroi'
 
 def le_mapa(numero)
     arquivo = "mapa#{numero}.txt"
@@ -11,24 +12,13 @@ def encontra_jogador(mapa)
     mapa.each_with_index do |linha_atual, linha|
         coluna_do_heroi = linha_atual.index caracter_do_heroi
         if coluna_do_heroi
-            return [linha, coluna_do_heroi]
+            heroi = Heroi.new
+            heroi.linha = linha
+            heroi.coluna = coluna_do_heroi
+            return heroi
         end
     end
     nil
-end
-
-def calcula_nova_posicao(heroi, direcao)
-    heroi = heroi.dup
-    movimentos = {
-        "W" => [-1, 0],
-        "S" => [+1, 0],
-        "A" => [0, -1],
-        "D" => [0, +1]
-    }
-    movimento = movimentos[direcao]
-    heroi[0] += movimento[0]
-    heroi[1] += movimento[1]
-    heroi
 end
 
 def posicao_valida?(mapa, posicao)
@@ -108,13 +98,13 @@ def joga(nome)
         direcao = pede_movimento
 
         heroi = encontra_jogador mapa
-        nova_posicao = calcula_nova_posicao heroi, direcao
-        if !posicao_valida? mapa, nova_posicao
+        nova_posicao = heroi.calcula_nova_posicao direcao
+        if !posicao_valida? mapa, nova_posicao.to_array
             next
         end
 
-        mapa[heroi[0]][heroi[1]] = " "
-        mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+        mapa[heroi.linha][heroi.coluna] = " "
+        mapa[nova_posicao.linha][nova_posicao.coluna] = "H"
 
         mapa = move_fantasmas mapa
         if jogador_perdeu?(mapa)
